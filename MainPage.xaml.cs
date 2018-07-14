3f8e54dc22ffe2df2a05f38af8eb86326935de2b
Harald Heide Gundersen <harald.heide.gundersen@hotmail.com>
2018-07-14 11:01:04 +0200

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,108 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Threading.Tasks;
+
+using BuildAzure.IoT.Adafruit.BME280;
+
+using Windows.UI.Xaml;
+using Windows.UI.Core;
+using Windows.Storage;
+using Windows.Devices.Gpio;
+using Windows.UI.Xaml.Controls;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Windows.ApplicationModel;
+
+namespace SmartDevice
+{
+    public sealed partial class MainPage : Page
+    {
+        private DispatcherTimer measureTimer;
+
+        // temperature sensor
+        private BME280Sensor bme280Sensor = new BME280Sensor();
+
+        private WebApiClient webApiClient = new WebApiClient();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Application.Current.Suspending += Current_Suspending;
+            // set up timer to sample proximity and temperature values every 1000 milliseconds
+            measureTimer = new DispatcherTimer();
+            measureTimer.Interval = TimeSpan.FromMilliseconds(1000);
+            // attach event handler on each 1000ms tick
+            measureTimer.Tick += MeasureTimerTick;
+
+            // initialize GPIO devices
+            InitializeDevices();
+        }
+
+        private  void Current_Suspending(object sender, SuspendingEventArgs e)
+        {
+             webApiClient.DeleteDevice();
+        }
+
+
+        #region Methods
+        private async void InitializeDevices()
+        {
+            #region Old
+            //var gpio = GpioController.GetDefault();
+            //// show an error if there is no GPIO controller
+            //if (gpio is null)
+            //{
+            //    //interruptPin = null;
+            //    GpioStatus.Text = "There is no GPIO controller on this device.";
+            //    return;
+            //}
+            // set up interrupt pin for proximity sensor
+            //interruptPin = gpio.OpenPin(intPinNumber);
+
+            //// pull up interrupt pin as sensor will pull down to notify
+            //interruptPin.SetDriveMode(GpioPinDriveMode.InputPullUp);
+
+            // initialize BME280 Sensor + Intitialize VNCL4010
+            //await Task.WhenAll(bme280Sensor.Initialize(), vncl4010Sensor.Initialize()); //hhe
+            #endregion Old
+
+            await Task.WhenAll(bme280Sensor.Initialize());
+
+            GpioStatus.Text = "Connecting to IoT Hub...";
+
+            //await IoTHub.ConnectToIoTHub();  //hhe
+            //RunAsync().GetAwaiter().GetResult();
+            await webApiClient.PostDevice();
+
+            GpioStatus.Text = "";
+
+            // start measuring temperature and proximity 
+            measureTimer.Start();
+        }
+        #endregion Methods
+
+        #region Handlers
+        private async void MeasureTimerTick(object sender, object e)
+        {
+            // read Temperature
+            double temperature = await bme280Sensor.ReadTemperature();
+            double humidity = await bme280Sensor.ReadHumidity();
+            double pressure = await bme280Sensor.ReadPressure();
+            // convert to Fahrenheit
+            //double fahrenheitTemperature = temperature * 1.8 + 32.0; // hhe
+
+            // read Proximity
+            //int proximity = vncl4010Sensor.ReadProximity(); //hhe
+
+            //TemperatureStatus.Text = "The temperature is currently " + fahrenheitTemperature.ToString("n1") + "°F"; //hhe
+            TemperatureStatus.Text = "The temperature is currently " + temperature.ToString("n1") + "°C";
+            HumidityStatus.Text = "The humidity is currently " + humidity.ToString("n1") + "%";
+            PressureStatus.Text = "The pressure is currently " + pressure.ToString("n1") + "";
+
+            await webApiClient.PutDevice(temperature,humidity,pressure);
+        }
+        #endregion
+    }
+}
+
---
+++ WebApiClient.cs
@@ -0,0 +1,152 @@
+﻿// needs nuget webAPI client for .net
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+
+namespace SmartDevice
+{
+    public class Device
+    {
+        public string Id { get; set; }
+        public string Location { get; set; }
+        public string Name { get; set; }
+        public string X { get; set; }
+        public string Y { get; set; }
+        public string Z { get; set; }
+        public string Type { get; set; }
+        public string Temperature { get; set; }
+        public string Humidity { get; set; }
+        public string Pressure { get; set; }
+    }
+
+    public class WebApiClient
+    {
+        private string UrlDevice_PathAndQuery = "";
+        private string DeviceId = "";
+        private HttpClient client = new HttpClient();
+
+        private Device device;
+        public async Task PostDevice()
+        {
+            // Update port # in the following line.
+            client.BaseAddress = new Uri("http://192.168.0.2/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+                device = new Device
+                {
+                    Location = "Office",
+                    Name = "IoT01",
+                    X = "0.0",
+                    Y = "0.0",
+                    Z = "0.0",
+                    Type = "BME820",
+                    Temperature = "0.0",
+                    Humidity = "0.0",
+                    Pressure = "0.0"
+                };
+
+                // Create a new device POST
+                var urlDevice = await CreateDeviceAsync(device);
+                Console.WriteLine($"Created at {urlDevice}");
+
+                // Get the device
+                device = await GetDeviceAsync(urlDevice.PathAndQuery);
+                DeviceId = device.Id;
+                UrlDevice_PathAndQuery = urlDevice.PathAndQuery;
+                //ShowDevice(device);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public async Task PutDevice(double temperature, double humidity, double pressure)
+        {
+            if(UrlDevice_PathAndQuery == "")
+            {
+                return;
+            }
+            device = await GetDeviceAsync(UrlDevice_PathAndQuery);
+
+            device.Temperature = temperature.ToString("n1");
+            device.Humidity = humidity.ToString("n1");
+            device.Pressure = pressure.ToString("n1");
+            await UpdateDeviceAsync(device);
+        }
+
+        //public async Task DeleteDevice()
+        //{
+        //    if (DeviceId == "")
+        //    {
+        //        return;
+        //    }
+        //    await DeleteDeviceAsync(DeviceId);
+        //}
+        public void DeleteDevice()
+        {
+            if (UrlDevice_PathAndQuery == "")
+            {
+                return;
+            }
+             DeleteDevice(DeviceId);
+        }
+
+        private void ShowDevice(Device device)
+        {
+            Console.WriteLine($"Location: {device.Location}\t" + 
+                "Name: " +  $"{device.Name}\t" + 
+                "Type: {device.Type}");
+        }
+
+        private async Task<Uri> CreateDeviceAsync(Device device)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync("api/devices/", device);
+            response.EnsureSuccessStatusCode();
+
+            // return URI of the created resource.
+            return response.Headers.Location;
+        }
+
+        private async Task<Device> GetDeviceAsync(string path)
+        {
+            Device device = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                device = await response.Content.ReadAsAsync<Device>();
+            }
+            return device;
+        }
+
+        private async Task<Device> UpdateDeviceAsync(Device device)
+        {
+            HttpResponseMessage response = await client.PutAsJsonAsync($"api/devices/{device.Id}", device);
+            response.EnsureSuccessStatusCode();
+
+            // Deserialize the updated product from the response body.
+            device = await response.Content.ReadAsAsync<Device>();
+            return device;
+        }
+
+        //private async Task<HttpStatusCode> DeleteDeviceAsync(string id)
+        //{
+        //    HttpResponseMessage response = await client.DeleteAsync($"api/devices/{id}");
+        //    return response.StatusCode;
+        //}
+        private void DeleteDevice(string id)
+        {
+            client.DeleteAsync($"api/devices/{id}");
+        }
+    }
+}
